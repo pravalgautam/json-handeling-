f801bf45961d3c0f803f9db5fb937e13ea8b25e4
pravalgautam <pravalgautam79@gmail.com>
2023-08-21 02:07:51 +0530

    first commit

--- json handling/ViewController.swift
+++ json handling/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var changeBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        getData()
+        
+        changeBtn.layer.cornerRadius = 20.0
+        
+        
     }
+    
+    func getData() {
+        let session = URLSession.shared
+        let serviceURL = URL(string: "https://meme-api.com/gimme")
+        
+        let task = session.dataTask(with: serviceURL!) { (serviceData, serviceResponse, error) in
+            if let error = error {
+                print("Error fetching data: \(error)")
+                return
+            }
+            
+            if let httpResponse = serviceResponse as? HTTPURLResponse, httpResponse.statusCode == 200 {
+                if let jsonData = try? JSONSerialization.jsonObject(with: serviceData!, options: []) as? [String: Any],
+                   let link = jsonData["url"] as? String,
+                   let imageURL = URL(string: link),
+                   let imageData = try? Data(contentsOf: imageURL),
+                   let image = UIImage(data: imageData) {
+                    DispatchQueue.main.async {
+                        self.imageView.image = image
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+                @IBAction func changeTapped(_ sender: Any) {
 
-
-}
-
+                    getData()
+                }
+            }
+            
+        
